a49a4fe2c85d8b644d10e87c0d379f03b72b681a
eliseocervantes <jireh.cervantes@utec.edu.pe>
2025-07-27 12:26:26 -0500

    Primer commit

---
+++ src/main/java/com/example/backendgygarquitectos/BackendGyGArquitectosApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendgygarquitectos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGyGArquitectosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendGyGArquitectosApplication.class, args);
+    }
+
+}
